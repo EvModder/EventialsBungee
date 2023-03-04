9e55fd30dc8d2dab7a119bb128f0f22bccb344a9
EvModder <evmodder@gmail.com>
2023-03-04 05:11:46 -0800

    TranslateComp join/quit messages

--- src/main/java/Eventials/listeners/PlayerJoinListener.java
+++ src/main/java/Eventials/listeners/PlayerJoinListener.java
@@ -1,32 +1,49 @@
 package Eventials.listeners;
 
-import EvLib.UUIDLookupUtil;
+import java.util.concurrent.TimeUnit;
 import Eventials.EventialsBungee;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.TranslatableComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class PlayerJoinListener implements Listener {
-	EventialsBungee plugin;
-	UUIDLookupUtil uuidFinder;
+public class PlayerJoinListener implements Listener{
+	final private EventialsBungee plugin;
 	public PlayerJoinListener(){
 		plugin = EventialsBungee.getPlugin();
 	}
 
 	@EventHandler
-	public void onPlayerJoin(PostLoginEvent evt) {
+	public void onPlayerJoin(ServerConnectedEvent evt){
 //		if(!evt.getConnection().getName().equals("MCI"))
+		if(evt.getPlayer().hasPermission("evbungee.silentjoin")) return;
 
-		if(!evt.getPlayer().hasPermission("evbungee.silentjoin")){
+		final String joinedServerName = evt.getServer().getInfo().getName();
+		plugin.getProxy().getScheduler().schedule(plugin, new Runnable(){@Override public void run(){
 			for(ProxiedPlayer p : plugin.getProxy().getPlayers()){
-				if(!p.getName().equals(evt.getPlayer().getName())){
-					p.sendMessage(new ComponentBuilder(
-							evt.getPlayer().getName()+" joined the game").color(ChatColor.GOLD).create());
+				if(p.getName().equals(evt.getPlayer().getName())) continue;
+	
+				TranslatableComponent joinMsg = new TranslatableComponent("multiplayer.player.joined");
+				if(p.getServer().getInfo().getName().equals(joinedServerName)){
+					// bungee selectors dont work (boooo)
+					//"@p[name="+evt.getPlayer().getName()+"]"
+					//@p
+					//evt.getPlayer().getUniqueId().toString()
+//					joinMsg.addWith(new SelectorComponent(evt.getPlayer().getUniqueId().toString()));
+//					p.sendMessage(new ComponentBuilder(joinMsg).color(ChatColor.YELLOW).create());
+				}
+				else{
+					joinMsg.addWith(new TextComponent(evt.getPlayer().getDisplayName()));
+					joinMsg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ChatColor.GOLD+"Server: "+ChatColor.DARK_GREEN+joinedServerName)));
+					p.sendMessage(new ComponentBuilder("["+joinedServerName.charAt(0)+"] ").color(ChatColor.GRAY).append(joinMsg).color(ChatColor.GRAY).create());
 				}
 			}
-		}
+		}}, 2000, TimeUnit.MILLISECONDS);
 	}
 }
--- src/main/java/Eventials/listeners/PlayerQuitListener.java
+++ src/main/java/Eventials/listeners/PlayerQuitListener.java
@@ -3,27 +3,44 @@ package Eventials.listeners;
 import Eventials.EventialsBungee;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.TranslatableComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class PlayerQuitListener implements Listener {
-	EventialsBungee plugin;
-	boolean rejoinLastServer;
+public class PlayerQuitListener implements Listener{
+	final private EventialsBungee plugin;
+	final boolean REJOIN_LAST_SERVER;
 
 	public PlayerQuitListener(){
 		plugin = EventialsBungee.getPlugin();
-		rejoinLastServer = plugin.getConfig().getBoolean("rejoin-last-server", true);
+		REJOIN_LAST_SERVER = plugin.getConfig().getBoolean("rejoin-last-server", true);
 	}
 
 	@EventHandler
-	public void onPlayerQuit(PlayerDisconnectEvent evt) {
-		if(!evt.getPlayer().hasPermission("evbungee.silentquit")){
-			plugin.getProxy().broadcast(
-					new ComponentBuilder(evt.getPlayer().getName()+" left the game").color(ChatColor.GOLD).create());
-		}
-		if(rejoinLastServer){
-			evt.getPlayer().setReconnectServer(evt.getPlayer().getServer().getInfo());
+	public void onPlayerQuit(PlayerDisconnectEvent evt){
+		final String quitServerName = evt.getPlayer().getServer().getInfo().getName();
+		if(REJOIN_LAST_SERVER) evt.getPlayer().setReconnectServer(evt.getPlayer().getServer().getInfo());
+		if(evt.getPlayer().hasPermission("evbungee.silentquit")) return;
+
+		for(ProxiedPlayer p : plugin.getProxy().getPlayers()){
+			if(p.getName().equals(evt.getPlayer().getName())) continue;
+
+			TranslatableComponent joinMsg = new TranslatableComponent("multiplayer.player.left");
+			if(p.getServer().getInfo().getName().equals(quitServerName)){
+				// bungee selectors don't work (boooo)
+//				joinMsg.addWith(new SelectorComponent(evt.getPlayer().getUniqueId().toString()));
+//				p.sendMessage(new ComponentBuilder(joinMsg).color(ChatColor.YELLOW).create());
+			}
+			else{
+				joinMsg.addWith(new TextComponent(evt.getPlayer().getDisplayName()));
+				joinMsg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ChatColor.GOLD+"Server: "+ChatColor.DARK_GREEN+quitServerName)));
+				p.sendMessage(new ComponentBuilder("["+quitServerName.charAt(0)+"] ").color(ChatColor.GRAY).append(joinMsg).color(ChatColor.GRAY).create());
+			}
 		}
 	}
 }
